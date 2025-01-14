78ae042ec864515a16845932ae72cae095c54738
MostafaMoradi7 <mostafa.moradi007172@gmail.com>
2025-01-14 19:05:19 +0330

    done.

--- tracking_system/settings.py
+++ tracking_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-x$9axjyegp@+qiuh2k%p4hjkx_u07(4%x1mlh2xnasp#b_il%a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
